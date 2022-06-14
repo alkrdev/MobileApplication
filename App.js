1c7357eb28f2bcf108aa79e0490a5fc202cd17f6
Alexander <sheepsaurus@gmail.coim>
2022-06-15 01:02:34 +0200

    Navigation hub

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import NavigationBar from "./src/components/NavigationBar"
+
+import Home from "./src/screens/Home"
+import Setup from "./src/screens/Setup"
+import Menu from "./src/screens/Menu"
+import Product from "./src/screens/Product"
+
+const Stack = createNativeStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1f2937',
+    accent: '#f1c40f',
+  },
+};
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator 
+          initialRouteName="Home"
+          mode="modal"
+          screenOptions={{
+            header: (props) => <NavigationBar {...props} />,
+          }}>
+          <Stack.Screen name="Home" options={{ title: 'Oversigt' }}>
+            {(props) => <Home {...props} />}
+          </Stack.Screen>
+          <Stack.Screen name="Setup" options={{ title: 'Opsætning' }}>
+            {(props) => <Setup {...props} />}
+          </Stack.Screen>
+          <Stack.Screen name="Menu" options={{ title: 'Menu' }}>
+            {(props) => <Menu {...props} />}
+          </Stack.Screen>
+          <Stack.Screen name="Product" options={{ title: 'Produkt' }}>
+            {(props) => <Product {...props} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
